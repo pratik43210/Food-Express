ffda5594016e877f8c30f89141b14e54fedf7478
Pratik Raja <pratik.raja.3847@outlook.com>
2023-06-14 18:04:15 +0530

    update

--- src/components/__tests__/sum.test.js
+++
@@ -1,9 +0,0 @@
-import { sum } from "../sum";
-// we get following error here while testing
-// SyntaxError: Cannot use import statement outside a module
-
-test("check sum of 2 positive numbers", ()=>{
-
-    expect(sum(2,5)).toBe(7);
-});
-
